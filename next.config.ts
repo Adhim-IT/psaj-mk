5562101c431c771974cf7a0c41635f7e11313e3f
Muhammad Luthfi <muhammad.lutfi@artajasa.co.id>
2025-04-04 01:09:48 +0700

    fix: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+  productionBrowserSourceMaps: false,
   images: {
     remotePatterns: [
       {
